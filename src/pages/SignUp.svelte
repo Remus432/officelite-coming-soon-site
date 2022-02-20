ccc984a1a9a5e7dd0072915adccb985bc4c24f16
remus432 <brosdev1@gmail.com>
2022-02-20 13:40:04 +0100

    scroll fix

--- src/pages/SignUp.svelte
+++ src/pages/SignUp.svelte
@@ -11,6 +11,7 @@
     description: "Easily manage your projects. Get on the list and receive in-app perks available only to early subscribers. We are moving into final development and getting ready for official launch soon."
   }
 
+  window.scrollY = 0
 </script>
 
 <div class="sign-up">
